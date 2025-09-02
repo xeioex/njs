3e255fc515f9cc833b0ebe8e03d0f4c2a3da6918
Dmitry Volyntsev <xeioex@nginx.com>
2025-09-02 15:42:24 -0700

    fixup! QuickJS: fix nginx configure with not configured njs module.

--- src/quickjs_adapted.h
+++ src/quickjs_adapted.h
@@ -10,8 +10,10 @@
 #if (defined(__GNUC__) && (__GNUC__ >= 8))                                    \
     || (defined(__clang__) && __has_warning("-Wcast-function-type"))
     #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wcast-function-type"
-        #include <quickjs.h>
+    #pragma GCC diagnostic ignored "-Wcast-function-type"
+
+    #include <quickjs.h>
+
     #pragma GCC diagnostic pop
 #else
     #include <quickjs.h>
