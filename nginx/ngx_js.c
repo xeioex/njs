504fda65609446133cd2c85cf2338d780f906145
Dmitry Volyntsev <xeioex@nginx.com>
2026-02-11 13:12:44 -0800

    Modules: js_set inline JavaScript expressions.
    Added support for inline JavaScript expressions in the js_set directive.
    Previously, js_set only accepted function references:
    
        js_set $var main.handler;
    
    Now it also accepts inline expressions:
    
        js_set $var '(r.uri)';
        js_set $var 'r.headersIn["Host"] || "none"';
    
    Additionally, nginx-style $variable references are expanded to
    the corresponding JavaScript variable access.  For example:
    
        js_set $var '$uri.toUpperCase()';
    
    is equivalent to:
    
        js_set $var 'r.variables.uri.toUpperCase()';
    
    In stream context, $var expands to s.variables.var.
    
    The expansion is a textual substitution at configuration parse time
    for $name patterns where name starts with [A-Za-z_].

--- nginx/ngx_js.c
+++ nginx/ngx_js.c
@@ -571,6 +571,7 @@ ngx_engine_njs_init(ngx_engine_t *engine, ngx_engine_opts_t *opts)
     vm_options.argc = ngx_argc;
     vm_options.init = 1;
 
+    vm_options.file.length = opts->file.length;
     vm_options.file.start = njs_mp_alloc(engine->pool, opts->file.length);
     if (vm_options.file.start == NULL) {
         return NGX_ERROR;
@@ -599,21 +600,107 @@ ngx_engine_njs_init(ngx_engine_t *engine, ngx_engine_opts_t *opts)
 }
 
 
+/*
+ * Parse line number from JS exception stack trace.
+ *
+ * njs format:
+ *   SyntaxError: ...\n    at /path:LINE\n
+ *
+ * QuickJS format:
+ *   SyntaxError: ...\n    at <main>:LINE:COL\n
+ *
+ * Returns 0 if no line number found.
+ */
+static ngx_uint_t
+ngx_js_error_line(u_char *start, size_t len)
+{
+    u_char      *p, *end;
+    ngx_uint_t   line;
+
+    end = start + len;
+
+    p = ngx_strlchr(start, end, '\n');
+    if (p == NULL) {
+        return 0;
+    }
+
+    p++;
+
+    while (p < end && *p == ' ') {
+        p++;
+    }
+
+    if (end - p < 3 || p[0] != 'a' || p[1] != 't' || p[2] != ' ') {
+        return 0;
+    }
+
+    p += 3;
+
+    /* find first ':' followed by a digit */
+
+    while (p < end && *p != '\n') {
+        if (*p == ':' && p + 1 < end && p[1] >= '0' && p[1] <= '9') {
+            p++;
+
+            line = 0;
+
+            while (p < end && *p >= '0' && *p <= '9') {
+                line = line * 10 + (*p - '0');
+                p++;
+            }
+
+            return line;
+        }
+
+        p++;
+    }
+
+    return 0;
+}
+
+
+static ngx_js_inline_t *
+ngx_js_inline_map(ngx_js_loc_conf_t *conf, u_char *start, size_t len)
+{
+    ngx_uint_t        i, line;
+    ngx_js_inline_t  *inl;
+
+    line = ngx_js_error_line(start, len);
+
+    if (line == 0 || conf->inlines == NGX_CONF_UNSET_PTR) {
+        return NULL;
+    }
+
+    i = line - 1;
+
+    if (conf->imports != NGX_CONF_UNSET_PTR) {
+        if (i < conf->imports->nelts) {
+            return NULL;
+        }
+
+        i -= conf->imports->nelts;
+    }
+
+    if (i >= conf->inlines->nelts) {
+        return NULL;
+    }
+
+    inl = conf->inlines->elts;
+
+    return &inl[i];
+}
+
+
 static ngx_int_t
 ngx_engine_njs_compile(ngx_js_loc_conf_t *conf, ngx_log_t *log, u_char *start,
     size_t size)
 {
-    u_char               *end;
-    njs_vm_t             *vm;
-    njs_int_t             rc;
-    njs_str_t             text;
-    ngx_uint_t            i;
-    njs_value_t          *value;
-    njs_opaque_value_t    exception, lvalue;
-    ngx_js_named_path_t  *import;
-
-    static const njs_str_t line_number_key = njs_str("lineNumber");
-    static const njs_str_t file_name_key = njs_str("fileName");
+    u_char              *end;
+    njs_vm_t            *vm;
+    njs_int_t            rc;
+    njs_str_t            text;
+    ngx_js_inline_t     *inl;
+    njs_opaque_value_t   exception;
 
     vm = conf->engine->u.njs.vm;
 
@@ -633,26 +720,16 @@ ngx_engine_njs_compile(ngx_js_loc_conf_t *conf, ngx_log_t *log, u_char *start,
         njs_vm_exception_get(vm, njs_value_arg(&exception));
         njs_vm_value_string(vm, &text, njs_value_arg(&exception));
 
-        value = njs_vm_object_prop(vm, njs_value_arg(&exception),
-                                   &file_name_key, &lvalue);
-        if (value == NULL) {
-            value = njs_vm_object_prop(vm, njs_value_arg(&exception),
-                                       &line_number_key, &lvalue);
+        inl = ngx_js_inline_map(conf, text.start, text.length);
+        if (inl != NULL) {
+            ngx_log_error(NGX_LOG_EMERG, log, 0, "%*s, included at %s:%ui",
+                          text.length, text.start, inl->file, inl->line);
 
-            if (value != NULL) {
-                i = njs_value_number(value) - 1;
-
-                if (i < conf->imports->nelts) {
-                    import = conf->imports->elts;
-                    ngx_log_error(NGX_LOG_EMERG, log, 0,
-                                  "%*s, included in %s:%ui", text.length,
-                                  text.start, import[i].file, import[i].line);
-                    return NGX_ERROR;
-                }
-            }
+        } else {
+            ngx_log_error(NGX_LOG_EMERG, log, 0, "%*s",
+                          text.length, text.start);
         }
 
-        ngx_log_error(NGX_LOG_EMERG, log, 0, "%*s", text.length, text.start);
         return NGX_ERROR;
     }
 
@@ -963,9 +1040,11 @@ static ngx_int_t
 ngx_engine_qjs_compile(ngx_js_loc_conf_t *conf, ngx_log_t *log, u_char *start,
     size_t size)
 {
-    JSValue               code;
+    ngx_str_t             text;
+    JSValue               code, exception;
     JSContext            *cx;
     ngx_engine_t         *engine;
+    ngx_js_inline_t      *inl;
     ngx_js_code_entry_t  *pc;
 
     engine = conf->engine;
@@ -975,8 +1054,34 @@ ngx_engine_qjs_compile(ngx_js_loc_conf_t *conf, ngx_log_t *log, u_char *start,
                    JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
 
     if (JS_IsException(code)) {
-        ngx_qjs_log_exception(engine, log, "compile");
+        exception = JS_GetException(cx);
+
+        if (ngx_qjs_dump_obj(engine, exception, &text) == NGX_OK) {
+            inl = ngx_js_inline_map(conf, text.data, text.len);
+            if (inl != NULL) {
+                ngx_log_error(NGX_LOG_EMERG, log, 0, "%V, included at %s:%ui",
+                              &text, inl->file, inl->line);
+
+            } else {
+                ngx_log_error(NGX_LOG_EMERG, log, 0, "js compile: %V", &text);
+            }
+
+        } else {
+            ngx_log_error(NGX_LOG_EMERG, log, 0, "js compile error");
+        }
+
+        JS_FreeValue(cx, exception);
         return NGX_ERROR;
+    }
+
+    if (engine->precompiled == NULL) {
+        engine->precompiled = njs_arr_create(engine->pool, 4,
+                                             sizeof(ngx_js_code_entry_t));
+        if (engine->precompiled == NULL) {
+            JS_FreeValue(cx, code);
+            ngx_log_error(NGX_LOG_EMERG, log, 0, "njs_arr_create() failed");
+            return NGX_ERROR;
+        }
     }
 
     pc = njs_arr_add(engine->precompiled);
@@ -3668,6 +3773,219 @@ ngx_js_init_preload_vm(njs_vm_t *vm, ngx_js_loc_conf_t *conf)
 }
 
 
+ngx_int_t
+ngx_js_is_function_ref(ngx_str_t *str)
+{
+    u_char  *p, *end;
+
+    p = str->data;
+    end = p + str->len;
+
+    if (p == end) {
+        return 0;
+    }
+
+    for ( ;; ) {
+        if ((*p < 'a' || *p > 'z')
+            && (*p < 'A' || *p > 'Z')
+            && *p != '_' && *p != '$')
+        {
+            return 0;
+        }
+
+        p++;
+
+        while (p < end && *p != '.') {
+            if ((*p < 'a' || *p > 'z')
+                && (*p < 'A' || *p > 'Z')
+                && (*p < '0' || *p > '9')
+                && *p != '_' && *p != '$')
+            {
+                return 0;
+            }
+
+            p++;
+        }
+
+        if (p == end) {
+            return 1;
+        }
+
+        /* skip '.' */
+        p++;
+
+        if (p == end) {
+            return 0;
+        }
+    }
+}
+
+
+static ngx_int_t
+ngx_js_set_inline(ngx_conf_t *cf, ngx_array_t **inlines, ngx_uint_t *index,
+    ngx_str_t *code, const char *arg, ngx_str_t *fname)
+{
+    u_char           *p, *end, *dst, *start, ch;
+    size_t            len, arg_len, extra;
+    ngx_str_t         expanded;
+    ngx_uint_t        nvars;
+    ngx_js_inline_t  *inl;
+
+    if (*inlines == NGX_CONF_UNSET_PTR) {
+        *inlines = ngx_array_create(cf->pool, 4, sizeof(ngx_js_inline_t));
+        if (*inlines == NULL) {
+            return NGX_ERROR;
+        }
+    }
+
+    inl = ngx_array_push(*inlines);
+    if (inl == NULL) {
+        return NGX_ERROR;
+    }
+
+    /* expand $var references to <arg>.variables.<var> */
+
+    arg_len = ngx_strlen(arg);
+
+    /* extra = strlen("<arg>.variables.") - strlen("$") */
+    extra = arg_len + sizeof(".variables.") - 1 - 1;
+
+    nvars = 0;
+    p = code->data;
+    end = p + code->len;
+
+    while (p < end) {
+        if (*p == '$' && p + 1 < end) {
+            ch = p[1];
+
+            if ((ch >= 'a' && ch <= 'z')
+                || (ch >= 'A' && ch <= 'Z')
+                || ch == '_')
+            {
+                nvars++;
+                p += 2;
+
+                while (p < end
+                       && ((*p >= 'a' && *p <= 'z')
+                           || (*p >= 'A' && *p <= 'Z')
+                           || (*p >= '0' && *p <= '9')
+                           || *p == '_'))
+                {
+                    p++;
+                }
+
+                continue;
+            }
+        }
+
+        p++;
+    }
+
+    if (nvars > 0) {
+        len = code->len + nvars * extra;
+
+        start = ngx_pnalloc(cf->pool, len);
+        if (start == NULL) {
+            return NGX_ERROR;
+        }
+
+        dst = start;
+        p = code->data;
+
+        while (p < end) {
+            if (*p == '$' && p + 1 < end) {
+                ch = p[1];
+
+                if ((ch >= 'a' && ch <= 'z')
+                    || (ch >= 'A' && ch <= 'Z')
+                    || ch == '_')
+                {
+                    p++;
+
+                    dst = ngx_cpymem(dst, arg, arg_len);
+                    dst = ngx_cpymem(dst, ".variables.",
+                                     sizeof(".variables.") - 1);
+
+                    while (p < end
+                           && ((*p >= 'a' && *p <= 'z')
+                               || (*p >= 'A' && *p <= 'Z')
+                               || (*p >= '0' && *p <= '9')
+                               || *p == '_'))
+                    {
+                        *dst++ = *p++;
+                    }
+
+                    continue;
+                }
+            }
+
+            *dst++ = *p++;
+        }
+
+        expanded.data = start;
+        expanded.len = dst - start;
+
+    } else {
+        expanded = *code;
+    }
+
+    inl->code = expanded;
+    inl->file = cf->conf_file->file.name.data;
+    inl->line = cf->conf_file->line;
+    inl->arg.len = arg_len;
+    inl->arg.data = (u_char *) arg;
+
+    inl->fname.data = ngx_pnalloc(cf->pool, sizeof("__js_set_65535") - 1);
+    if (inl->fname.data == NULL) {
+        return NGX_ERROR;
+    }
+
+    inl->fname.len = ngx_sprintf(inl->fname.data, "__js_set_%ui", (*index)++)
+                      - inl->fname.data;
+
+    *fname = inl->fname;
+
+    return NGX_OK;
+}
+
+
+ngx_int_t
+ngx_js_set_init(ngx_conf_t *cf, ngx_array_t **inlines, ngx_uint_t *index,
+    ngx_str_t *handler, const char *arg, ngx_js_set_t *set)
+{
+    u_char  *p, *end;
+
+    set->flags = 0;
+    set->file_name = cf->conf_file->file.name.data;
+    set->line = cf->conf_file->line;
+
+    if (ngx_js_is_function_ref(handler)) {
+
+        p = handler->data;
+        end = p + handler->len;
+
+        while (p < end) {
+            if (*p == '$' && p + 1 < end
+                && ((p[1] >= 'a' && p[1] <= 'z')
+                    || (p[1] >= 'A' && p[1] <= 'Z')
+                    || p[1] == '_'))
+            {
+                goto inline_expr;
+            }
+
+            p++;
+        }
+
+        set->fname = *handler;
+        return NGX_OK;
+    }
+
+inline_expr:
+
+    return ngx_js_set_inline(cf, inlines, index, handler, arg, &set->fname);
+}
+
+
 /*
  * Merge configuration values used at configuration time.
  */
@@ -3686,10 +4004,14 @@ ngx_js_merge_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
 {
     ngx_str_t            *path, *s;
     ngx_uint_t            i;
-    ngx_array_t          *imports, *preload_objects, *paths;
+    ngx_array_t          *imports, *inlines, *preload_objects, *paths;
+    ngx_js_inline_t      *inl, *ili;
     ngx_js_named_path_t  *import, *pi, *pij, *preload;
 
-    if (prev->imports != NGX_CONF_UNSET_PTR && prev->engine == NULL) {
+    if ((prev->imports != NGX_CONF_UNSET_PTR
+         || prev->inlines != NGX_CONF_UNSET_PTR)
+        && prev->engine == NULL)
+    {
         /*
          * special handling to preserve conf->engine
          * in the "http" or "stream" section to inherit it to all servers
@@ -3703,6 +4025,7 @@ ngx_js_merge_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
     }
 
     if (conf->imports == NGX_CONF_UNSET_PTR
+        && conf->inlines == NGX_CONF_UNSET_PTR
         && conf->type == prev->type
         && conf->paths == NGX_CONF_UNSET_PTR
         && conf->preload_objects == NGX_CONF_UNSET_PTR)
@@ -3710,6 +4033,7 @@ ngx_js_merge_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
         if (prev->engine != NULL) {
             conf->preload_objects = prev->preload_objects;
             conf->imports = prev->imports;
+            conf->inlines = prev->inlines;
             conf->type = prev->type;
             conf->paths = prev->paths;
             conf->engine = prev->engine;
@@ -3791,6 +4115,43 @@ ngx_js_merge_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
         }
     }
 
+    if (prev->inlines != NGX_CONF_UNSET_PTR) {
+        if (conf->inlines == NGX_CONF_UNSET_PTR) {
+            conf->inlines = prev->inlines;
+
+        } else {
+            inlines = ngx_array_create(cf->pool, 4,
+                                       sizeof(ngx_js_inline_t));
+            if (inlines == NULL) {
+                return NGX_ERROR;
+            }
+
+            ili = prev->inlines->elts;
+
+            for (i = 0; i < prev->inlines->nelts; i++) {
+                inl = ngx_array_push(inlines);
+                if (inl == NULL) {
+                    return NGX_ERROR;
+                }
+
+                *inl = ili[i];
+            }
+
+            ili = conf->inlines->elts;
+
+            for (i = 0; i < conf->inlines->nelts; i++) {
+                inl = ngx_array_push(inlines);
+                if (inl == NULL) {
+                    return NGX_ERROR;
+                }
+
+                *inl = ili[i];
+            }
+
+            conf->inlines = inlines;
+        }
+    }
+
     if (prev->paths != NGX_CONF_UNSET_PTR) {
         if (conf->paths == NGX_CONF_UNSET_PTR) {
             conf->paths = prev->paths;
@@ -3827,7 +4188,9 @@ ngx_js_merge_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
         }
     }
 
-    if (conf->imports == NGX_CONF_UNSET_PTR) {
+    if (conf->imports == NGX_CONF_UNSET_PTR
+        && conf->inlines == NGX_CONF_UNSET_PTR)
+    {
         return NGX_OK;
     }
 
@@ -4155,6 +4518,7 @@ ngx_js_init_conf_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
     size_t                size;
     ngx_str_t            *m, file;
     ngx_uint_t            i;
+    ngx_js_inline_t      *inl;
     ngx_pool_cleanup_t   *cln;
     ngx_js_named_path_t  *import;
 
@@ -4164,14 +4528,34 @@ ngx_js_init_conf_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
 
     size = 0;
 
-    import = conf->imports->elts;
-    for (i = 0; i < conf->imports->nelts; i++) {
+    if (conf->imports != NGX_CONF_UNSET_PTR) {
+        import = conf->imports->elts;
 
-        /* import <name> from '<path>'; globalThis.<name> = <name>; */
+        for (i = 0; i < conf->imports->nelts; i++) {
 
-        size += sizeof("import  from '';") - 1 + import[i].name.len * 3
-                + import[i].path.len
-                + sizeof(" globalThis. = ;\n") - 1;
+            /* import <name> from '<path>'; globalThis.<name> = <name>; */
+
+            size += sizeof("import  from '';") - 1 + import[i].name.len * 3
+                    + import[i].path.len
+                    + sizeof(" globalThis. = ;\n") - 1;
+        }
+    }
+
+    if (conf->inlines != NGX_CONF_UNSET_PTR) {
+        inl = conf->inlines->elts;
+
+        for (i = 0; i < conf->inlines->nelts; i++) {
+
+            /*
+             * function <fname>(<arg>) { return (<code>); }
+             *     globalThis.<fname> = <fname>;\n
+             */
+
+            size += sizeof("function () { return (); }") - 1
+                    + sizeof(" globalThis. = ;\n") - 1
+                    + inl[i].fname.len * 3
+                    + inl[i].arg.len + inl[i].code.len;
+        }
     }
 
     start = ngx_pnalloc(cf->pool, size + 1);
@@ -4180,20 +4564,44 @@ ngx_js_init_conf_vm(ngx_conf_t *cf, ngx_js_loc_conf_t *conf,
     }
 
     p = start;
-    import = conf->imports->elts;
-    for (i = 0; i < conf->imports->nelts; i++) {
 
-        /* import <name> from '<path>'; globalThis.<name> = <name>; */
+    if (conf->imports != NGX_CONF_UNSET_PTR) {
+        import = conf->imports->elts;
 
-        p = ngx_cpymem(p, "import ", sizeof("import ") - 1);
-        p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
-        p = ngx_cpymem(p, " from '", sizeof(" from '") - 1);
-        p = ngx_cpymem(p, import[i].path.data, import[i].path.len);
-        p = ngx_cpymem(p, "'; globalThis.", sizeof("'; globalThis.") - 1);
-        p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
-        p = ngx_cpymem(p, " = ", sizeof(" = ") - 1);
-        p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
-        p = ngx_cpymem(p, ";\n", sizeof(";\n") - 1);
+        for (i = 0; i < conf->imports->nelts; i++) {
+
+            /* import <name> from '<path>'; globalThis.<name> = <name>; */
+
+            p = ngx_cpymem(p, "import ", sizeof("import ") - 1);
+            p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
+            p = ngx_cpymem(p, " from '", sizeof(" from '") - 1);
+            p = ngx_cpymem(p, import[i].path.data, import[i].path.len);
+            p = ngx_cpymem(p, "'; globalThis.",
+                           sizeof("'; globalThis.") - 1);
+            p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
+            p = ngx_cpymem(p, " = ", sizeof(" = ") - 1);
+            p = ngx_cpymem(p, import[i].name.data, import[i].name.len);
+            p = ngx_cpymem(p, ";\n", sizeof(";\n") - 1);
+        }
+    }
+
+    if (conf->inlines != NGX_CONF_UNSET_PTR) {
+        inl = conf->inlines->elts;
+
+        for (i = 0; i < conf->inlines->nelts; i++) {
+            p = ngx_cpymem(p, "function ", sizeof("function ") - 1);
+            p = ngx_cpymem(p, inl[i].fname.data, inl[i].fname.len);
+            p = ngx_cpymem(p, "(", 1);
+            p = ngx_cpymem(p, inl[i].arg.data, inl[i].arg.len);
+            p = ngx_cpymem(p, ") { return (", sizeof(") { return (") - 1);
+            p = ngx_cpymem(p, inl[i].code.data, inl[i].code.len);
+            p = ngx_cpymem(p, "); } globalThis.",
+                           sizeof("); } globalThis.") - 1);
+            p = ngx_cpymem(p, inl[i].fname.data, inl[i].fname.len);
+            p = ngx_cpymem(p, " = ", sizeof(" = ") - 1);
+            p = ngx_cpymem(p, inl[i].fname.data, inl[i].fname.len);
+            p = ngx_cpymem(p, ";\n", sizeof(";\n") - 1);
+        }
     }
 
     *p = '\0';
@@ -4288,6 +4696,7 @@ ngx_js_create_conf(ngx_conf_t *cf, size_t size)
     conf->paths = NGX_CONF_UNSET_PTR;
     conf->type = NGX_CONF_UNSET_UINT;
     conf->imports = NGX_CONF_UNSET_PTR;
+    conf->inlines = NGX_CONF_UNSET_PTR;
     conf->preload_objects = NGX_CONF_UNSET_PTR;
 
     conf->reuse = NGX_CONF_UNSET_SIZE;
---
+++ nginx/t/js_inline.t
@@ -0,0 +1,174 @@
+#!/usr/bin/perl
+
+# (C) Dmitry Volyntsev
+# (C) F5, Inc.
+
+# Tests for http njs module, js_set inline expressions.
+
+###############################################################################
+
+use warnings;
+use strict;
+
+use Test::More;
+
+use Socket qw/ CRLF /;
+
+BEGIN { use FindBin; chdir($FindBin::Bin); }
+
+use lib 'lib';
+use Test::Nginx;
+
+###############################################################################
+
+select STDERR; $| = 1;
+select STDOUT; $| = 1;
+
+my $t = Test::Nginx->new()->has(qw/http rewrite/)->plan(14);
+
+use constant TEMPLATE_CONF => <<'EOF';
+
+%%TEST_GLOBALS%%
+
+daemon off;
+
+events {
+}
+
+http {
+    %%TEST_GLOBALS_HTTP%%
+
+    js_set $expr    '(r.uri)';
+    js_set $header  'r.headersIn["Foo"] || "none"';
+    js_set $sum     '1 + 2';
+    js_set $var     'r.variables.test_var.toUpperCase()';
+
+    js_set $expanded_uri '$uri';
+    js_set $expanded_complex '$uri.toUpperCase() + "_" + ($arg_a || "none")';
+
+    %%EXTRA_CONF%%
+
+    js_set $test_var     test.variable;
+
+    js_import test.js;
+
+    server {
+        listen       127.0.0.1:8080;
+        server_name  localhost;
+
+        location /inline {
+            return 200 "uri=$expr foo=$header sum=$sum";
+        }
+
+        location /var {
+            return 200 "var=$var";
+        }
+
+        location /mixed {
+            return 200 "test_var=$test_var uri=$expr";
+        }
+
+        location /expanded {
+            return 200 "uri=$expanded_uri complex=$expanded_complex";
+        }
+    }
+}
+
+EOF
+
+###############################################################################
+
+$t->write_file('test.js', <<EOF);
+    function variable(r) {
+        return 'from_func';
+    }
+
+    export default {variable};
+
+EOF
+
+write_conf($t, '');
+
+$t->try_run('no njs');
+
+###############################################################################
+
+like(http_get('/inline'), qr/uri=\/inline/, 'inline uri');
+like(http_get('/inline'), qr/foo=none/, 'inline headerIn');
+like(http(
+	'GET /inline HTTP/1.0' . CRLF
+	. 'Foo: bar' . CRLF
+	. 'Host: localhost' . CRLF . CRLF
+), qr/foo=bar/, 'inline headerIn with header');
+
+like(http_get('/inline'), qr/sum=3/, 'inline sum');
+like(http_get('/var'), qr/var=FROM_FUNC/, 'inline var func ref');
+like(http_get('/mixed'), qr/test_var=from_func/, 'mixed func ref');
+like(http_get('/mixed'), qr/uri=\/mixed/, 'mixed inline');
+
+like(http_get('/expanded'), qr/uri=\/expanded/, 'expanded $uri');
+like(http_get('/expanded'), qr/complex=\/EXPANDED_none/,
+	'expanded $var complex');
+like(http_get('/expanded?a=X'), qr/complex=\/EXPANDED_X/,
+	'expanded $var complex with arg');
+
+$t->stop();
+
+###############################################################################
+
+like(check($t, "js_set \$bad 'return 1';"),
+	qr/SyntaxError.*included at.*nginx\.conf:29/s,
+	'inline syntax error location');
+
+like(check($t, "js_set \$bad '1 +';"),
+	qr/SyntaxError.*included at.*nginx\.conf:29/s,
+	'inline syntax error unexpected end');
+
+$t->write_file('bad.js', 'export default {INVALID SYNTAX');
+
+like(check($t, 'js_import bad.js;'),
+	qr/\[emerg\].*SyntaxError/s,
+	'file syntax error');
+
+unlike(check($t, 'js_import bad.js;'),
+	qr/included at/s,
+	'file syntax error no inline location');
+
+open my $fh, '>', $t->testdir() . '/error.log';
+close $fh;
+
+###############################################################################
+
+sub write_conf {
+	my ($t, $extra) = @_;
+
+	$t->write_file_expand('nginx.conf',
+		TEMPLATE_CONF =~ s/%%EXTRA_CONF%%/$extra/r);
+}
+
+sub check {
+	my ($t, $extra) = @_;
+
+	$t->stop();
+	unlink $t->testdir() . '/error.log';
+
+	write_conf($t, $extra);
+
+	eval {
+		open OLDERR, ">&", \*STDERR; close STDERR;
+		$t->run();
+		open STDERR, ">&", \*OLDERR;
+	};
+
+	return unless $@;
+
+	my $log = $t->read_file('error.log');
+
+	if ($ENV{TEST_NGINX_VERBOSE}) {
+		map { Test::Nginx::log_core($_) } split(/^/m, $log);
+	}
+
+	return $log;
+}
+
+###############################################################################
